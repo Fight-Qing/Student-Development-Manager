702a74de8d4602cb3fb716bbc89d1fe551eae96c
ANDQE <2538634015@qq.com>
2023-03-06 19:32:07 +0800

    Initial commit

---
+++ src/main/java/com/wust/manager/StudentDevelopmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wust.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDevelopmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDevelopmentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wust/manager/StudentDevelopmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wust.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDevelopmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
